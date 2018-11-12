7691e0007d42af0938342d206ddcde1d36af0480
Liam Abrahams <l.abrahams@bath.ac.uk>
2018-11-12 17:41:55 +0000

    debug commit

--- conservation.py
+++ conservation.py
@@ -34,7 +34,7 @@ def get_conservation(transcript_list, output_file, max_dS_threshold = None, max_
     gen.create_output_directories(temp_dir)
     # get a list of the transcript ids
     transcript_ids = list(transcript_list.keys())
-    transcript_ids = transcript_ids[:100]
+    transcript_ids = transcript_ids[:1000]
     # output_filelist = run_conservation_check(transcript_ids, transcript_list, max_dS_threshold, max_omega_threshold, temp_dir)
     outputs = gen.run_parallel_function(transcript_ids, [transcript_list, max_dS_threshold, max_omega_threshold, temp_dir], run_conservation_check)
     # remove the old output file if there is one
