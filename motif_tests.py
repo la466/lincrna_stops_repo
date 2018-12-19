001a302fb9cb67e02682f9a42daf34ba95b9cbb6
Liam Abrahams <l.abrahams@bath.ac.uk>
2018-12-19 12:03:49 +0000

    transfer

--- motif_tests.py
+++ motif_tests.py
@@ -13,20 +13,20 @@ import os
 
 def main():
 
-    arguments = ["output_directory", "required_simulations", "motif_codon_densities"]
+    arguments = ["output_directory", "motif_file", "required_simulations", "motif_codon_densities"]
 
     description = ""
-    args = gen.parse_arguments(description, arguments, flags = [2], ints=[1])
-    output_directory, required_simulations, motif_codon_densities = args.output_directory, args.required_simulations, args.motif_codon_densities
+    args = gen.parse_arguments(description, arguments, flags = [3], ints=[2])
+    output_directory, motif_file, required_simulations, motif_codon_densities = args.output_directory, args.motif_file, args.required_simulations, args.motif_codon_densities
 
-    motif_file = "source_data/motif_sets/int3.txt"
-    motif_controls_directory = "{0}/int3_dinucleotide_controls".format(output_directory)
+
+    motif_controls_directory = "{0}/{1}_dinucleotide_controls".format(output_directory, motif_file.split("/")[-1].split(".")[0])
     gen.create_output_directories(motif_controls_directory)
 
     test_output_directory = "{0}/motif_tests".format(output_directory)
     gen.create_output_directories(test_output_directory)
 
-    output_file = "{0}/int3_densities.csv".format(test_output_directory)
+    output_file = "{0}/{1}_densities.csv".format(test_output_directory, motif_file.split("/")[-1].split(".")[0])
     codon_combinations_file = "{0}/codon_combinations.txt".format(output_directory)
     if motif_codon_densities:
 
