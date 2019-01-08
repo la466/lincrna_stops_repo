98734455cf4d1e0feb2d5943c8bc50bf94b32b56
Liam Abrahams <l.abrahams@bath.ac.uk>
2019-01-08 08:11:41 +0000

    transfer

--- purine_content.R
+++ purine_content.R
@@ -2,8 +2,6 @@ file = read.csv("clean_run/purine_content.csv", head=T)
 
 head(file)
 
-shapiro.test(file$exon_purine_content)
-
 qqnorm(file$exon_purine_content)
 qqnorm(file$intron_purine_content)
 
@@ -13,4 +11,14 @@ median(file$intron_purine_content)
 
 cor.test(file$exon_purine_content, file$intron_purine_content, method = "spearman")
 
-tail(file)
+wilcox.test(file$exon_core_purine_content, file$intron_core_purine_content, paired = T)
+median(file$exon_core_purine_content)
+median(file$intron_core_purine_content)
+
+
+file = read.csv("temp_files/random_purine_content.csv", head = T)
+head(file)
+
+wilcox.test(file$exon, file$intron, paired = T)
+wilcox.test(file$sim_exon, file$sim_intron, paired = T)
+
